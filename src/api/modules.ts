b65c50e94fb0bf019d352cc48da176897e0bc90d
Ana Tomboulian <ana.tomboulian@gmail.com>
2020-05-24 00:19:12 +0000

    Static module framework

---
+++ src/api/modules.ts
@@ -0,0 +1,35 @@
+import Module from 'types/modules';
+import { InitialModule } from 'types/modules';
+
+export default {
+    get(id: string): Module {
+        // Axios or fetch call will go here once backend is proven out,
+        // and return a promise instead.
+        if (id === 'SgPw7783b5hD8yrQx4Z7cL') {
+            return {
+                id: 'SgPw7783b5hD8yrQx4Z7cL',
+                name: 'HTML',
+                blurb: 'The structure for building the functional foundation of your website',
+                articles: [
+                    {
+                        id: 'KxSPU75ts6tRgPpXYbh4di',
+                        name: 'Wikipedia - HTML',
+                        blurb:
+                            'Hypertext Markup Language (HTML) is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.',
+                        link: 'https://en.wikipedia.org/wiki/HTML',
+                    },
+                ],
+                videos: [
+                    {
+                        id: 'c45fTz9AmA523mXiPWjkBE',
+                        name: 'Learn HTML in 12 Minutes',
+                        blurb:
+                            'In this short HTML tutorial, I explain the basic structure of an HTML webpage and introduce some important tags.',
+                        link: 'https://www.youtube.com/watch?v=bWPMSSsVdPk',
+                    },
+                ],
+            };
+        }
+        return InitialModule;
+    },
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,15 +5,19 @@ import Grid from '@material-ui/core/Grid';
 // App Components
 import Banner from '../components/Banner';
 import Dendrogram from '../components/Dendrogram';
+import Module from 'components/Module';
 
 const Home: FunctionComponent = () => {
     return (
         <Grid container spacing={3}>
-            <Grid item xs={12}>
+            <Grid item md={12}>
                 <Banner title="Welcome to SolveStack" subtitle="The most beautiful place online to learn to code" />
             </Grid>
-            <Grid item xs={12}>
+            <Grid item xs={12} md={8}>
                 <Dendrogram />
+            </Grid>
+            <Grid item xs={12} md={4}>
+                <Module />
             </Grid>
         </Grid>
     );
