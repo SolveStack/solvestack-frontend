b8b484afe2399a5904ae8b66457e92de7bb177a7
Tom Boolean <tomboolean@gmail.com>
2020-09-03 21:39:51 -0700

    solution for searching on enter key

--- src/pages/ContactUsPlaceholder.tsx
+++ src/pages/ContactUsPlaceholder.tsx
@@ -7,7 +7,7 @@ const PlaceHolder: FunctionComponent = () => {
     return (
         <Banner
             title="Contact Us page is coming soon!"
-            subtitle="Reach us at ana.tomboulian@gmail.com or bluefire2121@gmail.com for questions and comments. We'd love to hear from you!"
+            subtitle="Reach us at ana.tomboulian@gmail.com for questions and comments. We'd love to hear from you!"
         />
     );
 };
