96533ed3dbdd155e6a77e9ac26c87f8f8e64e59b
chrissyalbert <chrissy_albert@yahoo.com>
2020-09-19 13:52:30 -0700

    work on glossary

--- src/api/glossary.ts
+++ src/api/glossary.ts
@@ -9,4 +9,7 @@ export default {
     list(): Promise<AxiosResponse<Term[]>> {
         return axios.get<Array<Term>>('http://localhost:8000/terms/');
     },
+    search(search: string): Promise<AxiosResponse<Term[]>> {
+        return axios.get<Array<Term>>(`https://localhost:8000/terms/?name=${search}`);
+    }
 };
--- src/pages/Glossary.tsx
+++ src/pages/Glossary.tsx
@@ -2,6 +2,9 @@ import React, { FunctionComponent, useEffect, useState } from 'react';
 // Material UI
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/core/Autocomplete';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 // Components
 import TermCard from 'components/TermCard';
 // API
@@ -9,7 +12,17 @@ import api from 'api';
 
 import Term, { initialTermData } from 'types/glossary';
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        search: {
+            position: 'absolute',
+            right: 20,
+        },
+    }),
+);
+
 const Glossary: FunctionComponent = () => {
+    const classes = useStyles();
     const [terms, setTerms] = useState<Term[]>([{ ...initialTermData }]);
     useEffect(() => {
         function loadData(): void {
@@ -20,12 +33,33 @@ const Glossary: FunctionComponent = () => {
         loadData();
     }, []);
 
+    const search = (query: string): void => {
+        api.glossary.search(query).then((response) => {
+            setTerms(response.data);
+        });
+    }
     return (
         <>
             <Box>
                 <Typography component="h2" variant="h2">
                     Glossary
                 </Typography>
+                <Autocomplete
+        freeSolo
+        id="free-solo-2-demo"
+        disableClearable
+        options={terms.map((term) => term.name)}
+        renderInput={(params): JSX => (
+          <TextField
+            {...params}
+            label="Search input"
+            margin="normal"
+            variant="outlined"
+            InputProps={{ ...params.InputProps, type: 'search' }}
+          />
+        )}
+      />
+                <TextField className={classes.search} id="standard-basic"></TextField>
             </Box>
             {terms && terms.map((term) => <TermCard key={term.id} term={term}></TermCard>)}
         </>
