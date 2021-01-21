824b23221ecd0dbb7efb3b84794268c0ef7b4d64
chrissyalbert <chrissy_albert@yahoo.com>
2021-01-20 17:32:15 -0700

    Fix error in Testimonials with import Typography and make h1 consistent across pages

--- src/pages/Testimonials.tsx
+++ src/pages/Testimonials.tsx
@@ -2,16 +2,17 @@
 import React, { FunctionComponent } from 'react';
 // Material UI Components
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 // Material-UI Styles
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 
 import { reviews, ReviewSquare } from '../components/Reviews';
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
     testimonials: {
       alignItems: 'stretch'
     },
-  });
+  }));
 
 const Testimonials: FunctionComponent = () => {
     const classes = useStyles();
@@ -19,8 +20,8 @@ const Testimonials: FunctionComponent = () => {
         <Grid container spacing={3} className={classes.testimonials} >
             <Grid item xs={12} >
                 <header>
-                <Typography>
-                    <h1>Testimonials</h1>
+                <Typography component="h1" variant="h1">
+                    Testimonials
                 </Typography>
                 </header>
           </Grid>
